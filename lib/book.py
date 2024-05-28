9e4548296f333b1bc7ecf6df1797bd26c39487e6
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-05-28 17:37:06 +0300

    ngumu sana

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
     
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    def page_count(self, value):
+         if value != int:
+            print ("page_count must be an integer\n")
+         else:
+             self.page_count = value
+
+    def page_count(self):
+        return self._page_count
+      
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    
+    
+
+    
+    
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    # def requires_int(self,  page_count):
+    #     if page_count != int():
+    #         print ("page_count must be an integer")
+       
